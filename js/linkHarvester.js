cffbbac8945928c1aef72db627c822b6b28dfcda
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-01-15 20:48:06 +0100

    retrieve links from uploaded file

---
+++ js/linkHarvester.js
@@ -0,0 +1,35 @@
+//https://scotch.io/tutorials/use-the-html5-file-api-to-work-with-files-locally-in-the-browser
+//https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+(function(){
+	function bindUiEvents() {
+		$('#file-upload').change( function() { 
+			console.log(this.files);
+
+			renderLinks(this.files[0]);
+		});
+	}
+
+	function renderLinks(file) {
+		const reader = new FileReader(),
+			$linksContainer = $('.links-container');
+
+		reader.onload = function(event) {
+			const text = event.target.result,
+				pattern = /(<a.+)(<\/a>)/gm,
+				links = text.match(pattern);
+
+			$linksContainer.empty();
+
+			links.forEach(link => {
+				$linksContainer.append(
+					`<li class='link-item'>${link}</li>`
+				)
+			});
+		}
+
+		reader.readAsText(file);
+	}
+
+	bindUiEvents();
+})();
